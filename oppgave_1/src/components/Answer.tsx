3e8646c685ab0bba107d4bf041f0ce60882919b0
sirsa4 <yaya.abbdi@gmail.com>
2023-11-22 19:24:59 +0100

    next and send buttons show fixed. Issue with attempt count tho

--- oppgave_1/src/components/Answer.tsx
+++ oppgave_1/src/components/Answer.tsx
@@ -9,21 +9,46 @@ import { TaskType } from "@/types"
 export default function Answer({
   current,
   data,
+  lastTask,
+  answer,
+  setAnswer,
 }: {
   current: TaskType
   data: string[]
+  lastTask: boolean
+  answer: number
+  setAnswer: any
 }) {
-  const [answer, setAnswer] = useState(12)
+  // const [answer, setAnswer] = useState(0)
   const [attempts, setAttempts] = useState(3)
 
-  const send = (event: MouseEvent<HTMLButtonElement>) => {
+  const send = async (event: MouseEvent<HTMLButtonElement>) => {
     event.preventDefault()
-    console.log(answer)
+    //console.log(answer)
+    const updatedTask = await fetch(`/api/restapi/${current.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id: current.id,
+        answer,
+        attempts,
+      }),
+    })
+    try {
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   const update = (event: FormEvent<HTMLInputElement>) => {
     setAnswer(parseInt(event.currentTarget.value))
+
+    //check if answer is not correct - meaning answer from user is not same as return value of calcuate()
     if (calculate(current, data) !== answer) {
+      //decrememnt number attempts by 1 each time wrong answer is given by users
+      //this is buggy since each keystroke is counted as an attemp. Example if correct answer is 12, that would atleast 1 attemp when user types numkey 1, then numkey 2 will also be another attempt.
       setAttempts((prev) => prev - 1)
       console.log("Attemps: " + attempts)
       if (attempts === 0) {
@@ -42,7 +67,7 @@ export default function Answer({
         onInput={update}
       />
       {calculate(current, data) === answer ? "Bra jobbet!" : null}
-      <button onClick={send}>Send</button>
+      {lastTask ? <button onClick={send}>Send</button> : null}
     </div>
   )
 }
--- oppgave_1/src/components/Progress.tsx
+++ oppgave_1/src/components/Progress.tsx
@@ -3,16 +3,21 @@
 import { useState } from "react"
 import type { MouseEvent } from "react"
 
+import { calculate } from "@/lib/utils"
 import { type TaskType } from "@/types"
 
 export default function Progress({
   current,
+  data,
   next,
   prev,
+  answer,
 }: {
   current: TaskType
+  data: string[]
   next: () => void
   prev: () => void
+  answer: number
 }) {
   //const [state, setState] = useState(0)
   //const currentTask = props.tasks[state]
@@ -21,11 +26,13 @@ export default function Progress({
     <footer className="mt-4 border-t-slate-300">
       <p>{current?.id}</p>
       <button onClick={prev} className="bg-purple-700 text-white">
-        Forrige
+        Forrige oppgave
       </button>
-      <button onClick={next} className="bg-teal-700 text-white">
-        Neste
-      </button>
+      {calculate(current, data) === answer ? (
+        <button onClick={next} className="bg-teal-700 text-white">
+          Neste oppgavke
+        </button>
+      ) : null}
     </footer>
   )
 }
