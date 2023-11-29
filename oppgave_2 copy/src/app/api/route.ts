e2a81e2baa68499c47908f30de85a32f36a1a8fd
sirsa4 <yaya.abbdi@gmail.com>
2023-11-29 19:54:21 +0100

    api route for users fixed. now we are using our own database

--- oppgave_2 copy/src/app/api/route.ts
+++ oppgave_2 copy/src/app/api/route.ts
@@ -1,13 +1,17 @@
-
-
 import { NextRequest, NextResponse } from "next/server"
 
+import { prisma } from "@/lib/prisma"
 
-export async function POST(request: NextRequest){
-    const body = await request.json()
-        
-    
-   // console.log(data);
-    return NextResponse.json({body})
-    
+export async function GET() {
+  try {
+    const res = await prisma.user.findMany()
+    return NextResponse.json({ data: res, status: 200 })
+  } catch (error) {}
+}
+
+export async function POST(request: NextRequest) {
+  const body = await request.json()
+
+  // console.log(data);
+  return NextResponse.json({ body })
 }
