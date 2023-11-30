c4d0ced6bf71cce4ba4986a4e2c586ecb138ffe2
MostafaAliHaider <90030448+MostafaAliHaider@users.noreply.github.com>
2023-11-30 22:08:54 +0100

    frontend create activity, able to print tags in console

---
+++ oppgave_2 copy/src/components/CreateActivity.tsx
@@ -0,0 +1,104 @@
+"use client"
+
+import React, { useState } from "react";
+
+const CreateSession = () => {
+  const [date, setDate] = useState("");
+  const [name, setName] = useState("");
+  const [tagsInput, setTagsInput] = useState([]);
+  const [activity, setActivity] = useState("");
+  const [intervals, setIntervals] = useState("");
+  const [sport, setSport] = useState("")
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+
+    const tags = tagsInput.split(",").map(tag => tag.trim());
+    // Her kan du utføre handlinger som å sende data til en API-endepunkt
+    const newSession = { date, name, tags, activity, intervals };
+    console.log("New Session:", newSession);
+
+    // Reset state etter innsending hvis nødvendig
+    setDate("");
+    setName("");
+    setTagsInput([]);
+    setActivity("");
+    setIntervals(0);
+  };
+
+  const sportsOptions = [
+    "running",
+    "cycling",
+    "triathlon",
+    "ski",
+    "swimming",
+    "weightlifting",
+    "other",
+  ]
+
+
+  return (
+    <div className="w-full max-w-xs">
+      <h1>Create New Session</h1>
+      <form onSubmit={handleSubmit} className="mb-4 rounded bg-white px-8 pb-8 pt-6 shadow-md">
+        <div className="mb-4">
+          <label className="mb-2 block text-sm font-bold text-gray-700">Date:</label>
+          <input
+            type="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label className="mb-2 block text-sm font-bold text-gray-700">Activity name:</label>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label className="mb-2 block text-sm font-bold text-gray-700">Tags:</label>
+          <input
+            type="text"
+            value={tagsInput}
+            onChange={(e) => setTagsInput(e.target.value)}
+            placeholder="Enter tags separated by commas"
+          />
+        </div>
+        <label className="mb-2 block text-sm font-bold text-gray-700">
+            Select a sport:
+            <select value={sport} onChange={(e) => setSport(e.target.value)}>
+              {sportsOptions.map((sport, index) => (
+                <option key={index} value={sport}>
+                  {sport}
+                </option>
+              ))}
+            </select>
+          </label>
+        <div className="mb-4">
+          <label className="mb-2 block text-sm font-bold text-gray-700">Intervals:</label>
+          <input
+            type="number"
+            value={intervals}
+            onChange={(e) => setIntervals(parseInt(e.target.value))}
+            required
+          />
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            type="submit"
+            className="focus:shadow-outline rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
+          >
+            Create Session
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default CreateSession;
