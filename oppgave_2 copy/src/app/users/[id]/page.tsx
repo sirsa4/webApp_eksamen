03355e850d771fdbfe462c0c078349cb1eddff0a
Dilbrino <89363171+Dilbrino@users.noreply.github.com>
2023-12-04 18:26:28 +0100

    adjustement on css and ssesion

--- oppgave_2 copy/src/app/users/[id]/page.tsx
+++ oppgave_2 copy/src/app/users/[id]/page.tsx
@@ -14,63 +14,69 @@ const Userpage = async ({ params }: { params: { id: string } }) => {
   console.log("id: " + params.id)
 
   return (
-    <>
+    <div className="grid space-y-8 text-center">
+  <div className="inline-grid items-center my-8 border-2 border-solid border-gray-100 m-4 rounded p-4 shadow-md">
+
+    <section className="m-4 rounded p-4 shadow-md bg-blue-200">
+      <header>
+        <h1 className="text-2xl font-bold mb-4 text-center">User page for</h1>
+        <h2 className="mb-2 text-lg font-bold">Bruker: {result?.userId}</h2>
+      </header>
       <div>
-        <section>
-          <header>
-            <h2>User page for: {result?.userId}</h2>
-          </header>
-          <div>
-            <p>ID: {params?.id}</p>
-            <p>Gender: {result?.gender}</p>
-            <p>Sport: {result?.sport}</p>
-          </div>
-        </section>
-        <section>
-          <header>
-            <h3>Activities</h3>
-          </header>
-          {result.activities?.map((ac: Activity) => {
-            return (
-              <div key={ac.id}>
-                <h4>Date: {ac.date}</h4>
-                <p>ID: {ac.id}</p>
-                <p>Sport: {ac.sport}</p>
-                <div>
-                  <span>Tags: </span>
-                  {Array.isArray(ac.tags)
-                    ? ac.tags.map((ta) => {
-                        return (
-                          <span key={`${ta}+${Math.random() + 1}`}>
-                            dsd{ta}
-                          </span>
-                        )
-                      })
-                    : "no tags"}
-                  {ac.tags?.split(",").map((tag: string) => {
-                    return <span key={`${tag}+${Math.random}`}>{tag},</span>
-                  })}
-                </div>
-                {ac.intervals?.map((int) => {
-                  return (
-                    <div key={int.id}>
-                      <header>
-                        <h5>ID: {int.id}</h5>
-                      </header>
-                      <div>
-                        <p>Duration: {int.duration}</p>
-                        <p>Intensity: {int.intensity}</p>
-                      </div>
-                    </div>
-                  )
-                })}
-              </div>
-            )
-          })}
-        </section>
+        <p className="mb-2 text-lg font-bold">ID: {params?.id}</p>
+        <p className="mb-2 text-lg font-bold">Gender: {result?.gender}</p>
+        <p className="mb-2 text-lg font-bold">Sport: {result?.sport}</p>
       </div>
-      <Link href={"/users"}>GÃ¥ tilbake</Link>
-    </>
+    </section>
+
+    <div className="activity-box border-2 border-solid border-gray-100 m-4 rounded p-4  shadow-md "  > Activities
+      {result.activities?.map((ac: Activity) => (
+        <div className="activity-box border-2 border-solid border-gray-00 m-4 rounded p-4 shadow-md bg-blue-200" >
+        <div  key={ac.id}>
+          <h4>Date: {ac.date}</h4>
+          <p>ID: {ac.id}</p>
+          <p>Sport: {ac.sport}</p>
+          <div>
+            <p>Tags: </p>
+            {Array.isArray(ac.tags) ? (
+              ac.tags.map((ta) => (
+                <span key={`${ta}+${Math.random() + 1}`}>{ta}</span>
+              ))
+            ) : (
+              "no tags"
+            )}
+            {ac.tags?.split(",").map((tag: string) => (
+              <span  key={`${tag}+${Math.random}`}>{tag},</span>
+            ))}
+            </div>
+          </div>
+          {ac.intervals?.map((int) => (
+            <div className="activity-box border-2 border-solid border-gray-500  m-4 rounded p-4 shadow-md" key={int.id}>
+              <header>
+                <h5>ID: {int.id}</h5>
+              </header>
+              <div>
+                <p>Duration: {int.duration}</p>
+                <p>Intensity: {int.intensity}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+
+    <div className="items-center justify-center">
+      <Link
+        href={"/users"}
+        type="submit"
+        className="focus:shadow-outline rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none text-center"
+      >
+        Gå tilbake
+      </Link>
+    </div>
+  </div>
+</div>
+
   )
 }
 
--- oppgave_2 copy/src/components/CreateSession.tsx
+++ oppgave_2 copy/src/components/CreateSession.tsx
@@ -28,10 +28,12 @@ const CreateSession = () => {
   
   
   return (
-    <div className=" w-full max-w-xs">
-      <h1>Session target</h1>
-      <form onSubmit={handleSubmit} className="mb-4 rounded bg-white px-8 pb-8 pt-6 shadow-md">
-        <div className="mb-4">
+    
+    <div className="grid justify-items-center ">
+    <div className="w-full max-w-xs  ">
+      <h1 className="text-2xl font-bold mb-4 text-center">Session target</h1>
+      <form onSubmit={handleSubmit} className="mb-4 rounded bg-white px-8 pb-8 pt-6 shadow-md ">
+        <div className=" mb-4  ">
           <label className="mb-2 block text-sm font-bold text-gray-700">Session name:</label>
           <input
             type="text"
@@ -49,40 +51,44 @@ const CreateSession = () => {
             required
           />
         </div>
-    
-        
-        <label className="mb-2 block text-sm font-bold text-gray-700">Goal</label>
-             <input
-            type="number"
-            value={goal}
-            onChange={(e) => setGoal(e.target.value)}
-            placeholder="Enter a number"
-          />
 
-          <label className="mb-2 block text-sm font-bold text-gray-700">Comment</label>
-             <input
-            type="text"
-            value={comment}
-            onChange={(e) => setComment(e.target.value)}
-            placeholder="Enter comment"
-          />
-          <div className="flex items-center justify-between">
+
+        <label className="mb-2 block text-sm font-bold text-gray-700">Goal</label>
+        <input
+          type="number"
+          value={goal}
+          onChange={(e) => setGoal(e.target.value)}
+          placeholder="Enter a number"
+          className="mb-4"
+        />
+
+        <label className="mb-2 block text-sm font-bold text-gray-700">Comment</label>
+        <input
+          type="text"
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+          placeholder="Enter comment"
+          className="mb-4"
+        />
+        <div className="flex items-center justify-center mb-4">
           <button
             type="submit"
             className="focus:shadow-outline rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
           >
             Create new session target
           </button>
+
         </div>
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-center mb-4">
           <button
             type="submit"
-            className="focus:shadow-outline rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
+            className=" shadow-mdfocus:shadow-outline rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
           >
             Create session target
           </button>
         </div>
       </form>
+    </div>
     </div>
   );
 };
